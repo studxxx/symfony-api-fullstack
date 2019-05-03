8caca8a9c6076d4a076723d5da66e7f9df09eded
studxxx <stud181177@gmail.com>
2019-05-03 11:13:45 +0300

    Implementing UserChecker to verify if account is enabled, 87

--- src/Security/UserEnabledChecker.php
+++ src/Security/UserEnabledChecker.php
@@ -1,10 +1,42 @@
 <?php
 
-
 namespace App\Security;
 
+use App\Entity\User;
+use Symfony\Component\Security\Core\Exception\AccountStatusException;
+use Symfony\Component\Security\Core\Exception\DisabledException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
-class UserEnabledChecker
+class UserEnabledChecker implements UserCheckerInterface
 {
 
+    /**
+     * Checks the user account before authentication.
+     *
+     * @param UserInterface $user
+     * @throws AccountStatusException
+     * @throws DisabledException
+     */
+    public function checkPreAuth(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            return;
+        }
+
+        if (!$user->getEnabled()) {
+            throw new DisabledException();
+        }
+    }
+
+    /**
+     * Checks the user account after authentication.
+     *
+     * @param UserInterface $user
+     * @throws AccountStatusException
+     */
+    public function checkPostAuth(UserInterface $user)
+    {
+        // TODO: Implement checkPostAuth() method.
+    }
 }
