ef05629a96112a32b04a356802ea879f8b4714b4
studxxx <stud181177@gmail.com>
2019-06-02 18:21:31 +0300

    Installing Monolog library for application logging, 119

--- src/Security/UserConfirmationService.php
+++ src/Security/UserConfirmationService.php
@@ -5,6 +5,7 @@ namespace App\Security;
 use App\Exception\InvalidConfirmationTokenException;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 
 class UserConfirmationService
 {
@@ -16,24 +17,36 @@ class UserConfirmationService
      * @var EntityManagerInterface
      */
     private $entityManager;
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
 
-    public function __construct(UserRepository $repository, EntityManagerInterface $entityManager)
-    {
+    public function __construct(
+        UserRepository $repository,
+        EntityManagerInterface $entityManager,
+        LoggerInterface $logger
+    ) {
         $this->repository = $repository;
         $this->entityManager = $entityManager;
+        $this->logger = $logger;
     }
 
     public function confirmUser(string $confirmationToken)
     {
+        $this->logger->debug('Fetching user by confirmation token.');
         $user = $this->repository->findOneBy(['confirmationToken' => $confirmationToken]);
 
         // User was found by confirmation token
         if (!$user) {
+            $this->logger->debug('User by confirmation token not found.');
             throw new InvalidConfirmationTokenException();
         }
 
         $user->setEnabled(true);
         $user->setConfirmationToken(null);
         $this->entityManager->flush();
+
+        $this->logger->debug('Confirmed user by confirmation token.');
     }
 }
